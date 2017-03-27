b2143c0776749d34bf76dd3941c52b272fcadfc4
zhanggengwei <m13716920635@163.com>
2017-03-27 23:18:13 +0800

    正则表达式的用法

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.Manager.Beans.UserInfo;
+import com.reflect.StringPattern.StringPattern;
 import com.tools.DataManager;
 import com.tools.UserInfoManager;
 
@@ -6,14 +7,17 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println(DataManager.getInstance());
-        UserInfo userInfo = new UserInfo();
-        userInfo.setPhone("18863014572");
-        userInfo.setPassWord("zhongjie");
-        userInfo.setNickName("zhangsan");
-        userInfo.setRegion("+86");
-        UserInfoManager.registerUserInfo(userInfo);
-        System.out.println(UserInfoManager.LoginUserInfo("18863014571","zhongjie"));
+//        System.out.println(DataManager.getInstance());
+//        UserInfo userInfo = new UserInfo();
+//        userInfo.setPhone("18863014572");
+//        userInfo.setPassWord("zhongjie");
+//        userInfo.setNickName("zhangsan");
+//        userInfo.setRegion("+86");
+//        UserInfoManager.registerUserInfo(userInfo);
+//        System.out.println(UserInfoManager.LoginUserInfo("18863014571","zhongjie"));
+//
+
+        StringPattern.sortIPAddress();
 
     }
 }
---
+++ src/com/reflect/StringPattern/StringPattern.java
@@ -0,0 +1,80 @@
+package com.reflect.StringPattern;
+
+import java.util.TreeSet;
+
+/**
+ * Created by vd on 2017/3/27.
+ */
+public class StringPattern {
+    private static String numberRegex = "\\d";
+    private static String URLRegex = "";
+    private static String passWordRegex = "\\w";
+    private static String QQRegex = "^[1-9]\\d{4,14}";
+
+    public static Boolean isNumber(String regex)
+    {
+       return regex.matches(numberRegex);
+    }
+
+//    判断qq号码
+    public static Boolean isQQFormatter(String string)
+    {
+        return string.matches(QQRegex);
+    }
+
+
+    /*
+    *  // 分组
+    *  叠词
+    *  (.)//1+
+    *  String str = "zjlloooookkkk";
+    *  str.replaceAll("(.)\\1++","$1");"$1" 代表着第一组的数据
+    *  zjlok
+    *
+    *  18863014571
+    *  str.replaceAll("(\\d{3})\\d{4}(\\d{4})"),"$1****$2";
+    *  188****4571
+    *
+    *
+    *
+    * */
+
+    /*
+    * 1.治疗口吃，我要要要学习习习习习习程程程程程程程序
+    * 2 IP地址排序 192.168.10.34 [1-2]{3}\\.
+    * 3 邮箱地址的校验
+    * */
+
+   public static void sortIPAddress()
+   {
+       String address = "127.0.0.1  192.168.1.2 255.0.3.4 3.3.3.3";
+       //使用添零
+       address = address.replaceAll("(\\d+)","00$1");
+       address = address.replaceAll("0*(\\d{3})","$1");
+
+       String ip_arr[]= address.split(" +");
+       TreeSet<String> treeSet = new TreeSet<String>();
+       for (String str:ip_arr)
+       {
+           treeSet.add(str);
+       }
+       for (String str:treeSet) {
+           System.out.println(str.replaceAll("0*(\\d+)", "$1"));
+
+       }
+   }
+   public static Boolean emailAddress(String email)
+   {
+       Boolean isEmail = false;
+
+       String emailRegex = "[0-9a-zA-Z-]+@[0-9a-zA-Z]+\\.[a-zA-Z]{1,4}";
+
+
+
+
+
+       return  true;
+   }
+
+
+}
